4e3775a1463076bf7f9dfc5ff84bc936b5559d6b
Kim, Dae Ho <mis@mokwon.ac.kr>
2019-10-29 09:27:39 +0900

    Box Plot updata

--- R-for-BigData-Analysis/source/ch_5_146_Labelling_Boxplot.R
+++ R-for-BigData-Analysis/source/ch_5_146_Labelling_Boxplot.R
@@ -1,42 +1,81 @@
-# ch_5_146_Labelling_Boxplot
+# I. Box plot
 
-#----- p. 146 박스 플롯에 데이터 라벨달기**
+mag <- quakes$mag
 
-mag ＜- quakes$mag
+# change the plot window size : (7 x 7) => (4 X 7)
+win.graph(4,7)
 
+# 1. Box Plot
 boxstats <- boxplot(mag, 
-                    main="지진 발생 강도의 분포", 
-                    xlab="지진", 
+                    main="지진 발생 강도의 분포",
+                    xlab="지진 강도",
                     ylab="발생 건수",
                     col="red",
-                    notch=TRUE            # 박스의 허리를 들어가게
-                    )
+                    notch=TRUE)
 
-text( rep(1, NROW(boxstats$out)),       # x축으로 전체 박스 플롯
-      boxstats$out,                     # y축의 값을 기준으로 outlier를 출력할 데이터
-      labels=boxstats$out,              # y축의 값을 기준으로 outlier를 출력할 데이터의 라벨
-      pos=c(4,2,2,4,4,2,2)              # boxstats$out 의 데이터 갯수가 7개 이며, 2=왼쪽에, 4=오른쪽에 라벨 출력
+# 2. Display the points => points()
+len <- length(mag)           # the number of data
+points(mag, rep(1,len))      # position of points : (x, y=1) 
 
+# 3. Display fivenumbers => boxstats$stats
+#    ⇒ text(x, y, labels) 
+text(0.65, boxstats$stats, labels=boxstats$stats, pos=3, cex=0.8)   # cex=0.8 : size of tex
+                                                # pos = 1, 2, 3, 4
+
+# 4. display legend (Mean, Std Dev., n) ⇒ legend()
+# paste0()
+legend("topright", 
+        paste0(c("Mean", "Std Dev.", "n"), "=", 
+	    round(c(mean(mag), sd(mag), length(mag)), 3)), 
+	    text.col = 4,     # color of legend text 
+	    cex=0.8)          # size of legend text
+
+# 5. Display Outliers
+text( rep(1, NROW(boxstats$out)),
+             boxstats$out,
+             labels = boxstats$out,
+             pos = c(4,2,2,4,4,2,2),
+             cex = 0.8,
+             col = "red"
 )
 
-#---- 데이터 라벨 달기 끝
 
-#----- p. 146 박스 플롯을 수평으로 하고, 데이터 라벨 달기
+# II. horizontal box plot
 
-mag ＜- quakes$mag
+mag <- quakes$mag
+
+# change the plot window size : (7 x 7) => (7 X 4)
+win.graph(7,4)
+
+# 1. Horizontal Box Plot
 boxstats <- boxplot(mag, 
-                    main="지진 발생 강도의 분포", 
-                    xlab="발생 건수",                                  # x축의 라벨 변경
-                    ylab="지진",                                          # y 축의 라벨 변경
+                    main="지진 발생 강도의 분포",
+                    xlab="지진 강도",
+                    ylab="발생 건수",
                     col="red",
-                    horizontal=TRUE,      # 박스 플롯을 수평으로
-                    notch=TRUE               # 박스의 허리를 들어가게
-                    )
+                    notch=TRUE,
+                    horizontal=T)
 
-text( boxstats$out,                                    # x 축 변경
-      rep(1, NROW(boxstats$out)),             # y 축 변경
-      labels=boxstats$out, 
-      pos=c(3,1,1,3,3,1,1)                           # 1=아랫쪽, 3=윗쪽에 라벨 출력 
+# 2. Display the points => points()
+len <- length(mag)           # the number of data
+points(mag, rep(1,len))      # position of points : (x, y=1) 
+
+# 3. Fivenumbers => boxstats$stats
+text(boxstats$stats, 0.75, labels=boxstats$stats, pos=4, cex=0.8)   # cex=0.8 : size of tex
+                                              # pos = 1, 2, 3, 4
+
+# 4. display legend (Mean, Std Dev., n) ⇒ legend()
+# paste0()
+legend("topright", 
+        paste0(c("Mean", "Std Dev.", "n"), "=", 
+	    round(c(mean(mag), sd(mag), length(mag)), 3)), 
+	    text.col = 4,     # color of legend text 
+	    cex=0.8)          # size of legend text
+
+# 5. Displaying outliers
+text( boxstats$out, rep(1, NROW(boxstats$out)),
+                    labels = boxstats$out,
+                    pos = c(3,1,1,3,3,1,1), 
+                    cex=0.8, 
+                    col="red"
 )
-
-#---- 데이터 라벨 달기 끝
